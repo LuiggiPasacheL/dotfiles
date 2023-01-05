caaec297575d7f2cabe5e3a476a36d3eaf137a92
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-01-05 18:18:55 -0500

    chore: Add navigation

--- nvim/lua/neovim/maps.lua
+++ nvim/lua/neovim/maps.lua
@@ -4,12 +4,6 @@ local nnoremap = require('global.keymap').nnoremap
 nnoremap("<leader>w", "<cmd>w<CR>")
 nnoremap("<leader>q", "<cmd>q<CR>")
 
--- Navigation
-nnoremap("<C-h>", "<C-w>h")
-nnoremap("<C-j>", "<C-w>j")
-nnoremap("<C-k>", "<C-w>k")
-nnoremap("<C-l>", "<C-w>l")
-
 -- NERDTree
 nnoremap('<C-A-e>', ':NvimTreeFocus<CR>')
 nnoremap('<leader>e', ':NvimTreeToggle<CR>')
