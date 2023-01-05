15d25cbf30c6ef589a4fc2fc60362b22c76edfa7
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-01-05 18:35:28 -0500

    fix: remove problematic navigation mappings Ctrl k and ctrl j

--- nvim/lua/common/maps.lua
+++ nvim/lua/common/maps.lua
@@ -33,8 +33,6 @@ vnoremap('<leader>P','"+P')
 
 -- Navigation
 nnoremap("<C-h>", "<C-w>h")
-nnoremap("<C-j>", "<C-w>j")
-nnoremap("<C-k>", "<C-w>k")
 nnoremap("<C-l>", "<C-w>l")
 
 -- Vim surround ( Replace "s" for "z")
