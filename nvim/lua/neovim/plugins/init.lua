9ddac11faafcccf61b04b6ceb439edda171aa666
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-01-08 20:42:21 -0500

    feat(nvim): change order to load plugins

--- nvim/lua/neovim/plugins/init.lua
+++ nvim/lua/neovim/plugins/init.lua
@@ -1,17 +1,17 @@
 
+require('neovim.plugins.nvimtree')
+
+require('neovim.plugins.telescope')
+
 require('neovim.plugins.lsp')
 
 require('neovim.plugins.cmp')
 
+require('neovim.plugins.todo')
+
 require('neovim.plugins.comment')
 
 require('neovim.plugins.autotag')
-
-require('neovim.plugins.nvimtree')
-
-require('neovim.plugins.todo')
-
-require('neovim.plugins.telescope')
 
 require('neovim.plugins.autopairs')
 
