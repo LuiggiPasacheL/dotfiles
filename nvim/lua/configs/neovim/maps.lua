bfafb3933f98d261ffb04ddfdeb79d53c48a037b
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-01-24 12:16:28 -0500

    chore(nvim): change ':' and '<cmd>' to '<CMD>'

--- nvim/lua/configs/neovim/maps.lua
+++ nvim/lua/configs/neovim/maps.lua
@@ -6,19 +6,19 @@ vim.keymap.set('n',"<C-k>", "<C-w>k")
 vim.keymap.set('n',"<C-j>", "<C-w>j")
 
 -- Save and exit files
-vim.keymap.set('n',"<leader>q", ":q<CR>")
-vim.keymap.set('n',"<leader>w", ":w<CR>")
+vim.keymap.set('n',"<leader>q", "<CMD>q<CR>")
+vim.keymap.set('n',"<leader>w", "<CMD>w<CR>")
 
 -- NvimTree
-vim.keymap.set('n','<C-A-e>', ':NvimTreeFocus<CR>')
-vim.keymap.set('n','<leader>e', ':NvimTreeFocus<CR>')
+vim.keymap.set('n','<C-A-e>', '<CMD>NvimTreeFocus<CR>')
+vim.keymap.set('n','<leader>e', '<CMD>NvimTreeFocus<CR>')
 
 -- Git
-vim.keymap.set('n','<leader>g', '<cmd>G<CR>')
+vim.keymap.set('n','<leader>g', '<CMD>G<CR>')
 
 -- Telescope
-vim.keymap.set('n','<C-p>', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n','<leader>b', '<cmd>Telescope buffers<cr>')
-vim.keymap.set('n','<C-s>', '<cmd>Telescope tresitter<cr>')
-vim.keymap.set('n','<C-f>', '<cmd>Telescope live_grep<cr>')
-vim.keymap.set('n','<leader>tt', ':Telescope ')
+vim.keymap.set('n','<C-p>', '<CMD>Telescope find_files<cr>')
+vim.keymap.set('n','<leader>b', '<CMD>Telescope buffers<cr>')
+vim.keymap.set('n','<C-s>', '<CMD>Telescope tresitter<cr>')
+vim.keymap.set('n','<C-f>', '<CMD>Telescope live_grep<cr>')
+vim.keymap.set('n','<leader>tt', '<CMD>Telescope ')
