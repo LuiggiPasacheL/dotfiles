87ef716d7b984d89e828dbd62818697c38a61cda
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2024-01-13 11:54:14 -0500

    fix(neovim): snippets not working lsp-zero config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -63,6 +63,10 @@ return {
 
         local cmp = require('cmp')
 
+        local cmp_format = require('lsp-zero').cmp_format()
+
+        require("luasnip.loaders.from_vscode").lazy_load()
+
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
         local cmp_mappings = cmp.mapping.preset.insert({
             ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -86,7 +90,11 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             sources = {
-                { name = 'nvim_lsp' }
+                { name = 'path' },
+                { name = 'nvim_lsp' },
+                { name = 'nvim_lua' },
+                { name = 'luasnip', keyword_length = 2 },
+                { name = 'buffer',  keyword_length = 3 },
             },
             mapping = cmp_mappings,
             snippet = {
@@ -94,6 +102,7 @@ return {
                     require('luasnip').lsp_expand(args.body)
                 end,
             },
+            formatting = cmp_format,
         })
 
         vim.diagnostic.config({
