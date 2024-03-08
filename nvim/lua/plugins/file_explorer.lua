6331921c5b79490e423feead90023fe7a914b490
LuiggiPasacheL <luiggi.pasache.lopera@gmail.com>
2024-03-08 08:06:12 -0500

    fix(nvim): Update nvim tree (deleted tag)

--- nvim/lua/plugins/file_explorer.lua
+++ nvim/lua/plugins/file_explorer.lua
@@ -2,7 +2,6 @@ return {
     {
         'nvim-tree/nvim-tree.lua',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
-        tag = 'nightly',
         config = function()
             require("nvim-tree").setup({
                 auto_reload_on_write = true,
