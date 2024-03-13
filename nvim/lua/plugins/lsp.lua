7be327476a6055793938c4ee38f0ceb170f2d2f1
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2024-03-13 00:08:53 -0500

    feat(nvim): improve autopairs config with cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -77,7 +77,14 @@ return {
             },
         })
 
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
         local cmp = require('cmp')
+
+        cmp.event:on(
+            'confirm_done',
+            cmp_autopairs.on_confirm_done()
+        )
 
         local cmp_format = require('lsp-zero').cmp_format()
 
