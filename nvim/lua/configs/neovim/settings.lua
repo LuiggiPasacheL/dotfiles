fd2fcedfd00a51481963fb6e95c9bc9a3baad7f9
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2024-03-28 15:35:22 -0500

    add dap nvim

--- nvim/lua/configs/neovim/settings.lua
+++ nvim/lua/configs/neovim/settings.lua
@@ -1,14 +1,21 @@
 vim.o.splitright       = true
+vim.opt.splitbelow     = true
 
 vim.opt.nu             = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn     = "yes"
 vim.opt.mouse          = 'a'
 vim.opt.background     = "dark"
+vim.opt.showmode       = false
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.inccommand = 'split'
 
 vim.opt.fillchars      = { fold = " " }
 vim.opt.foldmethod     = "indent"
-vim.opt.foldenable     = false
+vim.opt.foldenable     = true
 vim.opt.foldlevel      = 99
 
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
@@ -25,5 +32,17 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
         vim.defer_fn(f, 100)
     end
 })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
 
 vim.g.copilot_assume_mapped = true -- Tab to accept completion
