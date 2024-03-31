82c7930ed4bc715cd4cdf85416dab619b35ed4d4
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2024-03-31 01:01:44 -0500

    add load vscode launch js nvim-dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -39,8 +39,16 @@ return {
             },
         }
 
+
+        local function continue()
+            if vim.fn.filereadable(".vscode/launch.json") == 1 then
+                require("dap.ext.vscode").load_launchjs()
+            end
+            dap.continue()
+        end
+
         -- Basic debugging keymaps, feel free to change to your liking!
-        vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+        vim.keymap.set('n', '<F5>', continue, { desc = 'Debug: Start/Continue' })
         vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
         vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
         vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
@@ -80,6 +88,72 @@ return {
 
         -- Install golang specific config
         require('dap-go').setup()
+
+        dap.adapters.go = {
+            type = "server",
+            port = 38697,
+            executable = {
+                command = "dlv",
+                args = { "dap", "-l", "127.0.0.1:38697" },
+            },
+            enrich_config = function(finalConfig, on_config)
+                local final_config = vim.deepcopy(finalConfig)
+
+                -- Placeholder expansion for launch directives
+                local placeholders = {
+                    ["${file}"] = function(_)
+                        return vim.fn.expand("%:p")
+                    end,
+                    ["${fileBasename}"] = function(_)
+                        return vim.fn.expand("%:t")
+                    end,
+                    ["${fileBasenameNoExtension}"] = function(_)
+                        return vim.fn.fnamemodify(vim.fn.expand("%:t"), ":r")
+                    end,
+                    ["${fileDirname}"] = function(_)
+                        return vim.fn.expand("%:p:h")
+                    end,
+                    ["${fileExtname}"] = function(_)
+                        return vim.fn.expand("%:e")
+                    end,
+                    ["${relativeFile}"] = function(_)
+                        return vim.fn.expand("%:.")
+                    end,
+                    ["${relativeFileDirname}"] = function(_)
+                        return vim.fn.fnamemodify(vim.fn.expand("%:.:h"), ":r")
+                    end,
+                    ["${workspaceFolder}"] = function(_)
+                        return vim.fn.getcwd()
+                    end,
+                    ["${workspaceFolderBasename}"] = function(_)
+                        return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+                    end,
+                    ["${env:([%w_]+)}"] = function(match)
+                        return os.getenv(match) or ""
+                    end,
+                }
+
+                if final_config.envFile then
+                    local filePath = final_config.envFile
+                    for key, fn in pairs(placeholders) do
+                        filePath = filePath:gsub(key, fn)
+                    end
+
+                    for line in io.lines(filePath) do
+                        local words = {}
+                        for word in string.gmatch(line, "[^=]+") do
+                            table.insert(words, word)
+                        end
+                        if not final_config.env then
+                            final_config.env = {}
+                        end
+                        final_config.env[words[1]] = words[2]
+                    end
+                end
+
+                on_config(final_config)
+            end,
+        }
     end,
     cond = Not_vscode()
 }
